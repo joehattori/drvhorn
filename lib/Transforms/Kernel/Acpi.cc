7c1a9091384825fce039836f444a15d04f66e098
Joe Hattori <joe2ninja21@gmail.com>
2024-03-29 11:19:17 +0900

    kernel: acpi: split runOnModule() into multiple functions

--- lib/Transforms/Kernel/Acpi.cc
+++ lib/Transforms/Kernel/Acpi.cc
@@ -27,68 +27,11 @@ public:
   AcpiSetup(std::string entry) : ModulePass(ID), entry_point(entry) {}
 
   bool runOnModule(Module &M) override {
-    if (M.getFunction("main")) {
-      LOG("ACPI", errs() << "ACPI: Main already exists.\n");
+    Function *main = insertMain(M);
+    if (!main)
       return false;
-    }
-    assert(entry_point != "" && "entry-point not specified");
-    Function *entry = M.getFunction(entry_point);
-    assert(entry && "entry-point not found");
-    LLVMContext &ctx = M.getContext();
-
-    Type *i32Ty = Type::getInt32Ty(ctx);
-    ArrayRef<Type *> params;
-    Function *main = Function::Create(
-        FunctionType::get(i32Ty, params, false),
-        GlobalValue::LinkageTypes::ExternalLinkage, "main", &M);
-
-    IRBuilder<> B(ctx);
-    B.SetInsertPoint(BasicBlock::Create(ctx, "", main));
-
-    SmallVector<Value *, 16> args;
-    for (Argument &A : entry->args()) {
-      FunctionCallee ndf = getNondetFn(A.getType(), M);
-      args.push_back(B.CreateCall(ndf));
-    }
-
-    CallInst *call = B.CreateCall(entry, args);
-
-    Value *isFailure = B.CreateICmpNE(call, ConstantInt::get(i32Ty, 0));
-    BasicBlock *failBlock = BasicBlock::Create(ctx, "", main);
-    BasicBlock *errBlock = BasicBlock::Create(ctx, "", main);
-    BasicBlock *retBlock = BasicBlock::Create(ctx, "", main);
-
-    B.CreateCondBr(isFailure, failBlock, retBlock);
-
-    // build failure path
-    B.SetInsertPoint(failBlock);
-    StructType *acpiTableType =
-        StructType::getTypeByName(ctx, "struct.acpi_table_list");
-    GlobalVariable *acpiTable = M.getGlobalVariable("acpi_gbl_root_table_list");
-    Value *tablesPtr =
-        B.CreateStructGEP(acpiTableType, acpiTable, ACPI_TABLE_DESC_INDEX);
-    StructType *descType =
-        StructType::getTypeByName(ctx, "struct.acpi_table_desc");
-    Value *firstTableDescPtr =
-        B.CreateLoad(descType->getPointerTo(), tablesPtr);
-    Value *validationCountPtr =
-        B.CreateStructGEP(descType, firstTableDescPtr, VALIDATION_COUNT_INDEX);
-    Type *i16Ty = Type::getInt16Ty(ctx);
-    LoadInst *validationCount = B.CreateLoad(i16Ty, validationCountPtr);
-    Value *isZero = B.CreateICmpEQ(validationCount, ConstantInt::get(i16Ty, 0));
-    B.CreateCondBr(isZero, retBlock, errBlock);
-
-    // build error path
-    B.SetInsertPoint(errBlock);
-    FunctionCallee errFn = M.getOrInsertFunction(
-        "__VERIFIER_error", FunctionType::get(Type::getVoidTy(ctx), false));
-    B.CreateCall(errFn);
-    B.CreateRet(ConstantInt::get(i32Ty, 42));
-
-    // build success path
-    B.SetInsertPoint(retBlock);
-    B.CreateRet(ConstantInt::get(i32Ty, 42));
-
+    CallInst *call = insertEntryCall(M, main);
+    buildErrorPathAssertion(M, main, call);
     return true;
   }
 
@@ -120,6 +63,74 @@ private:
         makeNewNondetFn(M, *type, ndfn.size(), "verifier.nondet.");
     ndfn[type] = res;
     return res;
+  }
+
+  Function *insertMain(Module &M) {
+    if (M.getFunction("main")) {
+      LOG("ACPI", errs() << "ACPI: Main already exists.\n");
+      return nullptr;
+    }
+
+    Type *i32Ty = Type::getInt32Ty(M.getContext());
+    ArrayRef<Type *> params;
+    return Function::Create(FunctionType::get(i32Ty, params, false),
+                            GlobalValue::LinkageTypes::ExternalLinkage, "main",
+                            &M);
+  }
+
+  CallInst *insertEntryCall(Module &M, Function *main) {
+    BasicBlock *block = BasicBlock::Create(M.getContext(), "", main);
+    IRBuilder<> B(block);
+    assert(entry_point != "" && "entry-point not specified");
+    Function *entry = M.getFunction(entry_point);
+    assert(entry && "entry-point not found");
+    SmallVector<Value *, 16> args;
+    for (Argument &A : entry->args()) {
+      FunctionCallee ndf = getNondetFn(A.getType(), M);
+      args.push_back(B.CreateCall(ndf));
+    }
+    return B.CreateCall(entry, args);
+  }
+
+  void buildErrorPathAssertion(Module &M, Function *main, CallInst *entryCall) {
+    LLVMContext &ctx = M.getContext();
+    IRBuilder<> B(&main->back());
+    Type *i32Ty = Type::getInt32Ty(ctx);
+    Value *isFailure = B.CreateICmpNE(entryCall, ConstantInt::get(i32Ty, 0));
+    BasicBlock *failBlock = BasicBlock::Create(ctx, "", main);
+    BasicBlock *errBlock = BasicBlock::Create(ctx, "", main);
+    BasicBlock *retBlock = BasicBlock::Create(ctx, "", main);
+
+    B.CreateCondBr(isFailure, failBlock, retBlock);
+
+    // build failure path
+    B.SetInsertPoint(failBlock);
+    StructType *acpiTableType =
+        StructType::getTypeByName(ctx, "struct.acpi_table_list");
+    GlobalVariable *acpiTable = M.getGlobalVariable("acpi_gbl_root_table_list");
+    Value *tablesPtr =
+        B.CreateStructGEP(acpiTableType, acpiTable, ACPI_TABLE_DESC_INDEX);
+    StructType *descType =
+        StructType::getTypeByName(ctx, "struct.acpi_table_desc");
+    Value *firstTableDescPtr =
+        B.CreateLoad(descType->getPointerTo(), tablesPtr);
+    Value *validationCountPtr =
+        B.CreateStructGEP(descType, firstTableDescPtr, VALIDATION_COUNT_INDEX);
+    Type *i16Ty = Type::getInt16Ty(ctx);
+    LoadInst *validationCount = B.CreateLoad(i16Ty, validationCountPtr);
+    Value *isZero = B.CreateICmpEQ(validationCount, ConstantInt::get(i16Ty, 0));
+    B.CreateCondBr(isZero, retBlock, errBlock);
+
+    // build error path
+    B.SetInsertPoint(errBlock);
+    FunctionCallee errFn = M.getOrInsertFunction(
+        "__VERIFIER_error", FunctionType::get(Type::getVoidTy(ctx), false));
+    B.CreateCall(errFn);
+    B.CreateBr(retBlock);
+
+    // build success path
+    B.SetInsertPoint(retBlock);
+    B.CreateRet(ConstantInt::get(i32Ty, 42));
   }
 };
 
