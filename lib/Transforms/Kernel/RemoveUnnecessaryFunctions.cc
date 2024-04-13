04d020929489e397dc6721983c9699bad9f4c385
Joe Hattori <joe2ninja21@gmail.com>
2024-04-13 23:00:51 +0900

    kernel: RemoveUnnecessaryFunctions: remove llvm.compiler.used and insert an empty one

--- lib/Transforms/Kernel/RemoveUnnecessaryFunctions.cc
+++ lib/Transforms/Kernel/RemoveUnnecessaryFunctions.cc
@@ -1,7 +1,13 @@
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Operator.h"
 #include "llvm/IR/User.h"
+#include "llvm/IR/Verifier.h"
 #include "llvm/Pass.h"
+
+#define COMPILER_USED_NAME "llvm.compiler.used"
 
 using namespace llvm;
 
@@ -13,27 +19,37 @@ public:
   RemoveUnnecessaryFunctions() : ModulePass(ID) {}
 
   bool runOnModule(Module &M) override {
-    StringRef functions[] = {
-        "default_idle",
-    };
-    for (StringRef name : functions) {
-      Function *fn = M.getFunction(name);
-      fn->dropAllReferences();
-      fn->eraseFromParent();
-    }
+    verify(M);
 
     StringRef globalVarNames[] = {
         "sys_call_table",
     };
     for (StringRef name : globalVarNames) {
-      GlobalValue *v = M.getNamedValue(name);
+      GlobalVariable *v = M.getNamedGlobal(name);
       v->eraseFromParent();
     }
+
+    GlobalVariable *compilerUsed = M.getNamedGlobal(COMPILER_USED_NAME);
+    Type *ty = compilerUsed->getType();
+    compilerUsed->eraseFromParent();
+    // llvm.compiler.used seems to be required, so insert an empty value
+    M.getOrInsertGlobal(COMPILER_USED_NAME, ty->getPointerElementType());
+
+    verify(M);
     return true;
   }
 
   virtual StringRef getPassName() const override {
     return "RemoveUnnecessaryFunctions";
+  }
+
+private:
+  void verify(Module &M) {
+    if (verifyModule(M, &errs())) {
+      errs() << "Module verification failed\n";
+    } else {
+      errs() << "Module verification Ok\n";
+    }
   }
 };
 
