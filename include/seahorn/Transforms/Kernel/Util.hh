263594c9860dd755b12e07b2103c8f48feeb8358
Joe Hattori <joe2ninja21@gmail.com>
2024-07-11 11:47:44 +0900

    separate util function definition and declaration

--- include/seahorn/Transforms/Kernel/Util.hh
+++ include/seahorn/Transforms/Kernel/Util.hh
@@ -1,77 +1,19 @@
 #pragma once
 
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Module.h"
-#include "llvm/Support/Casting.h"
+#include "llvm/IR/Instructions.h"
 
-namespace {
-llvm::StringRef getStructName(llvm::StringRef name) {
-  size_t p = name.find('.');
-  if (p == llvm::StringRef::npos)
-    return name;
-  size_t q = name.find('.', p + 1);
-  if (q == llvm::StringRef::npos)
-    return name;
-  return name.substr(0, q);
-}
-
-bool equivTypes(const llvm::Type *t1, const llvm::Type *t2) {
-  if (t1 == t2)
-    return true;
-  const llvm::StructType *st1 = llvm::dyn_cast<llvm::StructType>(t1);
-  const llvm::StructType *st2 = llvm::dyn_cast<llvm::StructType>(t2);
-  if (!st1 || !st2 || !st1->hasName() || !st2->hasName())
-    return false;
-  llvm::StringRef p1 = getStructName(st1->getName());
-  llvm::StringRef p2 = getStructName(st2->getName());
-  return p1 == p2;
-}
-
-const llvm::Function *extractCalledFunction(const llvm::CallInst *call) {
-  return llvm::dyn_cast<llvm::Function>(
-      call->getCalledOperand()->stripPointerCasts());
-}
-
-llvm::Function *extractCalledFunction(llvm::CallInst *call) {
-  return llvm::dyn_cast<llvm::Function>(
-      call->getCalledOperand()->stripPointerCasts());
-}
-
+namespace seahorn {
+llvm::StringRef getStructName(llvm::StringRef name);
+bool equivTypes(const llvm::Type *t1, const llvm::Type *t2);
+const llvm::Function *extractCalledFunction(const llvm::CallInst *call);
+llvm::Function *extractCalledFunction(llvm::CallInst *call);
 static void collectCallUser(llvm::User *user,
-                            llvm::SmallVector<llvm::CallInst *, 16> &res) {
-  if (llvm::isa<llvm::Instruction>(user)) {
-    if (llvm::CallInst *call = llvm::dyn_cast<llvm::CallInst>(user))
-      res.push_back(call);
-  } else {
-    for (llvm::User *user : user->users()) {
-      collectCallUser(user, res);
-    }
-  }
-}
-
-static void
-collectCallUser(const llvm::User *user,
-                llvm::SmallVector<const llvm::CallInst *, 16> &res) {
-  if (llvm::isa<llvm::Instruction>(user)) {
-    if (const llvm::CallInst *call = llvm::dyn_cast<llvm::CallInst>(user))
-      res.push_back(call);
-  } else {
-    for (const llvm::User *user : user->users()) {
-      collectCallUser(user, res);
-    }
-  }
-}
-
-llvm::SmallVector<llvm::CallInst *, 16> getCallers(llvm::Function *fn) {
-  llvm::SmallVector<llvm::CallInst *, 16> res;
-  collectCallUser(fn, res);
-  return res;
-}
-
+                            llvm::SmallVector<llvm::CallInst *, 16> &res,
+                            llvm::DenseSet<llvm::User *> &visited);
+static void collectCallUser(const llvm::User *user,
+                            llvm::SmallVector<const llvm::CallInst *, 16> &res,
+                            llvm::DenseSet<const llvm::User *> &visited);
+llvm::SmallVector<llvm::CallInst *, 16> getCallers(llvm::Function *fn);
 llvm::SmallVector<const llvm::CallInst *, 16>
-getCallers(const llvm::Function *fn) {
-  llvm::SmallVector<const llvm::CallInst *, 16> res;
-  collectCallUser(fn, res);
-  return res;
-}
-} // namespace
+getCallers(const llvm::Function *fn);
+} // namespace seahorn
---
+++ lib/Transforms/Kernel/Util.cc
@@ -0,0 +1,83 @@
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Instructions.h"
+
+#include "seahorn/Transforms/Kernel/Util.hh"
+
+namespace seahorn {
+llvm::StringRef getStructName(llvm::StringRef name) {
+  size_t p = name.find('.');
+  if (p == llvm::StringRef::npos)
+    return name;
+  size_t q = name.find('.', p + 1);
+  if (q == llvm::StringRef::npos)
+    return name;
+  return name.substr(0, q);
+}
+
+bool equivTypes(const llvm::Type *t1, const llvm::Type *t2) {
+  if (t1 == t2)
+    return true;
+  const llvm::StructType *st1 = llvm::dyn_cast<llvm::StructType>(t1);
+  const llvm::StructType *st2 = llvm::dyn_cast<llvm::StructType>(t2);
+  if (!st1 || !st2 || !st1->hasName() || !st2->hasName())
+    return false;
+  llvm::StringRef p1 = getStructName(st1->getName());
+  llvm::StringRef p2 = getStructName(st2->getName());
+  return p1 == p2;
+}
+
+const llvm::Function *extractCalledFunction(const llvm::CallInst *call) {
+  return llvm::dyn_cast<llvm::Function>(
+      call->getCalledOperand()->stripPointerCasts());
+}
+
+llvm::Function *extractCalledFunction(llvm::CallInst *call) {
+  return llvm::dyn_cast<llvm::Function>(
+      call->getCalledOperand()->stripPointerCasts());
+}
+
+static void collectCallUser(llvm::User *user,
+                            llvm::SmallVector<llvm::CallInst *, 16> &res,
+                            llvm::DenseSet<llvm::User *> &visited) {
+  if (!visited.insert(user).second)
+    return;
+  if (llvm::isa<llvm::Instruction>(user)) {
+    if (llvm::CallInst *call = llvm::dyn_cast<llvm::CallInst>(user))
+      res.push_back(call);
+  } else {
+    for (llvm::User *user : user->users()) {
+      collectCallUser(user, res, visited);
+    }
+  }
+}
+
+static void collectCallUser(const llvm::User *user,
+                            llvm::SmallVector<const llvm::CallInst *, 16> &res,
+                            llvm::DenseSet<const llvm::User *> &visited) {
+  if (!visited.insert(user).second)
+    return;
+  if (llvm::isa<llvm::Instruction>(user)) {
+    if (const llvm::CallInst *call = llvm::dyn_cast<llvm::CallInst>(user))
+      res.push_back(call);
+  } else {
+    for (const llvm::User *user : user->users()) {
+      collectCallUser(user, res, visited);
+    }
+  }
+}
+
+llvm::SmallVector<llvm::CallInst *, 16> getCallers(llvm::Function *fn) {
+  llvm::SmallVector<llvm::CallInst *, 16> res;
+  llvm::DenseSet<llvm::User *> visited;
+  collectCallUser(fn, res, visited);
+  return res;
+}
+
+llvm::SmallVector<const llvm::CallInst *, 16>
+getCallers(const llvm::Function *fn) {
+  llvm::SmallVector<const llvm::CallInst *, 16> res;
+  llvm::DenseSet<const llvm::User *> visited;
+  collectCallUser(fn, res, visited);
+  return res;
+}
+} // namespace seahorn
